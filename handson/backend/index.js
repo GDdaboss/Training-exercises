c84b3c9e77378d60b1507b76b582f2a23ce336c3
GDdaboss <gddaboss@gmail.com>
2020-09-08 11:32:12 +0530

    added cors

--- handson/backend/index.js
+++ handson/backend/index.js
@@ -1,5 +1,5 @@
 require( './data/init' );
-
+const cors = require('cors');
 const path = require( 'path' );
 const express = require( 'express' );
 const articlesRouter = require( './routes/articles' );
@@ -14,7 +14,7 @@ app.use( '/', ( req, res, next ) => {
 
     console.log( 'Response sent at', (new Date()).toTimeString() );
 });
-
+app.use(cors());
 app.use( express.urlencoded() );
 app.use( express.json() );
 app.use( '/articles', articlesRouter );
